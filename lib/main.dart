b0b524322096d30082a84d75058b508154bb0c79
Somye <mahajan.somye@gmail.com>
2024-08-25 00:36:48 +0530

    Added home

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:bureau_base/resources/style.dart';
 import 'package:bureau_base/respository/local_repository.dart';
 import 'package:bureau_base/screens/home.dart';
-import 'package:bureau_base/screens/onboard.dart';
 import 'package:flutter/material.dart';
 import 'package:forui/forui.dart';
 
@@ -24,22 +23,34 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final theme = FThemeData.inherit(
+      colorScheme: FThemes.blue.light.colorScheme.copyWith(
+        primary: Style.primaryColor,
+      ),
+      typography: FThemes.blue.light.typography.copyWith(
+        defaultFontFamily: 'Roboto',
+      ),
+      style: FStyle(
+        borderRadius: BorderRadius.circular(16),
+      ),
+    );
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       builder: (context, child) => FTheme(
-        data: FThemeData.inherit(
-          colorScheme: FThemes.blue.light.colorScheme.copyWith(
-            primary: Style.primaryColor,
-          ),
-          typography: FThemes.blue.light.typography.copyWith(
-            defaultFontFamily: 'Roboto',
+        data: theme.copyWith(
+          cardStyle: theme.cardStyle.copyWith(
+            decoration: theme.cardStyle.decoration.copyWith(
+              borderRadius: BorderRadius.circular(16),
+            ),
           ),
         ),
         child: child!,
       ),
-      home: FScaffold(
+      home: const FScaffold(
         contentPad: false,
-        content: onBoardingDone ? const HomeScreen() : const OnBoardScreen(),
+        //content: onBoardingDone ? const HomeScreen() : const OnBoardScreen(),  //TODO -DO THIS
+        content: HomeScreen(),
       ),
     );
   }
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,10 +1,37 @@
+import 'package:bureau_base/resources/style.dart';
+import 'package:bureau_base/utils/screen_size.dart';
 import 'package:flutter/material.dart';
+import 'package:forui/forui.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Text("hshdshd");
+    return Column(
+      children: [
+        Container(
+          height: ScreenSize.getPercentOfHeight(context, 0.35),
+          width: ScreenSize.getPercentOfWidth(context, 1),
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: FCard(
+              title: Text("EH"),
+              style: context.theme.cardStyle.copyWith(
+                decoration: BoxDecoration(
+                  color: Style.primaryColor,
+                ),
+              ),
+            ),
+          ),
+        ),
+        Row(
+          children: [
+            FAssets.icons.mapPinned(),
+            const Text('New Delhi'),
+          ],
+        ),
+      ],
+    );
   }
 }
